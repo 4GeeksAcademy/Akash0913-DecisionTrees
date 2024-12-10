1916c074cf8e03e371118f42c63352e0fa6cd950
Akash0913 <44508860+Akash0913@users.noreply.github.com>
2024-12-10 00:46:55 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,87 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from pickle import dump
 
-# your code here
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv")
+print(total_data.head())
+total_data = total_data.drop_duplicates().reset_index(drop = True)
+print(total_data.head())
+# No normalization of variables is needed for training this model
+
+# Feature selection
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import train_test_split
+
+X = total_data.drop("Outcome", axis = 1)
+y = total_data["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+selection_model = SelectKBest(k = 7)
+selection_model.fit(X_train, y_train)
+
+selected_columns = X_train.columns[selection_model.get_support()]
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = selected_columns)
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = selected_columns)
+
+print(X_train_sel.head())
+print(X_test_sel.head())
+X_train_sel["Outcome"] = y_train.values
+X_test_sel["Outcome"] = y_test.values
+X_train_sel.to_csv("clean_train.csv", index = False)
+X_test_sel.to_csv("clean_test.csv", index = False)
+train_data = pd.read_csv("clean_train.csv")
+test_data = pd.read_csv("clean_test.csv")
+
+print(train_data.head())
+
+plt.figure(figsize=(12, 6))
+
+pd.plotting.parallel_coordinates(total_data, "Outcome", color = ("#E58139", "#39E581", "#8139E5"))
+
+plt.show()
+X_train = train_data.drop(["Outcome"], axis = 1)
+y_train = train_data["Outcome"]
+X_test = test_data.drop(["Outcome"], axis = 1)
+y_test = test_data["Outcome"]
+model = DecisionTreeClassifier(random_state = 42)
+print(model.fit(X_train, y_train))
+
+fig = plt.figure(figsize=(15,15))
+
+tree.plot_tree(model, feature_names = list(X_train.columns), class_names = ["0", "1", "2"], filled = True)
+
+plt.show()
+y_pred = model.predict(X_test)
+print(y_pred)
+
+print(accuracy_score(y_test, y_pred))
+hyperparams = {
+    "criterion": ["gini", "entropy"],
+    "max_depth": [None, 5, 10, 20],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
+
+grid = GridSearchCV(model, hyperparams, scoring = "accuracy", cv = 10)
+print(grid)
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+grid.fit(X_train, y_train)
+
+print(f"Best hyperparameters: {grid.best_params_}")
+model = DecisionTreeClassifier(criterion = "entropy", max_depth = 5, min_samples_leaf = 4, min_samples_split = 2, random_state = 42)
+print(model.fit(X_train, y_train))
+y_pred = model.predict(X_test)
+y_pred
+print(accuracy_score(y_test, y_pred))
